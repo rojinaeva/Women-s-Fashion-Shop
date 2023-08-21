a10a1c200501e39b09b532462f0c3b504dcb0bf9
rojinaeva <rojina.eva.cse@gmail.com>
2023-08-21 20:59:08 +0600

    solved correct quantity to the cart similar to localstorage

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -6,16 +6,19 @@ const Cart = (props) => {
     console.log(props.cart);
     let total=0;
     let ShippingCharge=0;
+    let quantity=0;
     for(const product of cart){
-        total=total+product.price;
-        ShippingCharge=ShippingCharge+product.shippingCharge;
+        product.quantity=product.quantity||1;
+        total=total+product.price*product.quantity;
+        ShippingCharge=ShippingCharge+product.shippingCharge*product.quantity;
+        quantity=quantity+product.quantity;
     }
     const tax=total*7/100;
     const grandTotal=total+ShippingCharge+tax;
     return (
         <div className='cart-container'>
             <h3>Order Summary</h3>
-            <p>seleted items:{cart.length}</p>
+            <p>seleted items:{quantity}</p>
             <p>Total Price:${total.toFixed(2)}</p>
             <p>Total Shipping Charge:${ShippingCharge.toFixed(2)}</p>
             <p>Tax:${tax.toFixed(2)}</p>
